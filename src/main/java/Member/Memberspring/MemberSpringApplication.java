36d41b34bf1b8ee49801cacb33a489c30e363a6a
seilylook@naver.com <seilylook@naver.com>
2021-05-14 15:28:47 +0900

    initiate project

---
+++ src/main/java/Member/Memberspring/MemberSpringApplication.java
@@ -0,0 +1,13 @@
+package Member.Memberspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Member/Memberspring/MemberSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package Member.Memberspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
