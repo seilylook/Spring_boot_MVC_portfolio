5bfb85f3c463af1f0f615c3e0d5ba920cb8f94c8
seilylook@naver.com <seilylook@naver.com>
2021-05-17 16:13:40 +0900

    make_controller_dependency_injection_springbean

---
+++ src/main/java/Member/Memberspring/controller/MemberController.java
@@ -0,0 +1,17 @@
+package Member.Memberspring.controller;
+
+import Member.Memberspring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService){
+        this.memberService = memberService;
+    }
+
+}
--- src/main/java/Member/Memberspring/repository/MemoryMemberRepository.java
+++ src/main/java/Member/Memberspring/repository/MemoryMemberRepository.java
@@ -1,9 +1,11 @@
 package Member.Memberspring.repository;
 
 import Member.Memberspring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+@Repository
 public class MemoryMemberRepository implements MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>();
