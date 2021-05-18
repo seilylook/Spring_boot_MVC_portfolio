16cbac696a1eab633fcc3fa4535c3d8ec1f5085e
seilylook@naver.com <seilylook@naver.com>
2021-05-18 19:22:39 +0900

    controller_member_page_create_search

--- src/main/java/Member/Memberspring/controller/MemberController.java
+++ src/main/java/Member/Memberspring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package Member.Memberspring.controller;
 
+import Member.Memberspring.domain.Member;
 import Member.Memberspring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -11,7 +17,19 @@ public class MemberController {
 
     @Autowired
     public MemberController(MemberService memberService){
+
         this.memberService = memberService;
     }
 
+    @GetMapping(value = "/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @GetMapping(value = "/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
 }
